d93a4446ef321af7fbde429713e94eaf07a847f7
Adhil <amazementtech521@gamil.com>
2022-05-03 03:14:24 +0530

    Signup & Signin page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,86 @@
-import { Fragment } from "react"
-import GlobalStyles from "./theme/GlobalStyles"
-import Navbar from "./components/navbar/Navbar"
-import Sidebar from "./components/navbar/Sidebar"
+import { Fragment } from "react";
+import Sidebar from "./components/navbar/Sidebar";
+import Navbar from "./components/navbar/Navbar";
+
+import GlobalStyles from "./theme/GlobalStyles";
+
 function App() {
   return (
     <Fragment>
       <GlobalStyles />
-          <Navbar/>
-          <Sidebar/>
-          <h1>hi</h1>
-          <p>n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-          <p>n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-          <p>n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-          <p>n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-
+      <Navbar />
+      <Sidebar />
+      <h1>hi</h1>
+      <p>
+        n publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.n publishing and graphic
+        design, Lorem ipsum is a placeholder text commonly used to demonstrate
+        the visual form of a document or a typeface without relying on
+        meaningful content. Lorem ipsum may be used as a placeholder before
+        final copy is available.n publishing and graphic design, Lorem ipsum is
+        a placeholder text commonly used to demonstrate the visual form of a
+        document or a typeface without relying on meaningful content. Lorem
+        ipsum may be used as a placeholder before final copy is available.n
+        publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
+      <p>
+        n publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.n publishing and graphic
+        design, Lorem ipsum is a placeholder text commonly used to demonstrate
+        the visual form of a document or a typeface without relying on
+        meaningful content. Lorem ipsum may be used as a placeholder before
+        final copy is available.n publishing and graphic design, Lorem ipsum is
+        a placeholder text commonly used to demonstrate the visual form of a
+        document or a typeface without relying on meaningful content. Lorem
+        ipsum may be used as a placeholder before final copy is available.n
+        publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
+      <p>
+        n publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.n publishing and graphic
+        design, Lorem ipsum is a placeholder text commonly used to demonstrate
+        the visual form of a document or a typeface without relying on
+        meaningful content. Lorem ipsum may be used as a placeholder before
+        final copy is available.n publishing and graphic design, Lorem ipsum is
+        a placeholder text commonly used to demonstrate the visual form of a
+        document or a typeface without relying on meaningful content. Lorem
+        ipsum may be used as a placeholder before final copy is available.n
+        publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
+      <p>
+        n publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.n publishing and graphic
+        design, Lorem ipsum is a placeholder text commonly used to demonstrate
+        the visual form of a document or a typeface without relying on
+        meaningful content. Lorem ipsum may be used as a placeholder before
+        final copy is available.n publishing and graphic design, Lorem ipsum is
+        a placeholder text commonly used to demonstrate the visual form of a
+        document or a typeface without relying on meaningful content. Lorem
+        ipsum may be used as a placeholder before final copy is available.n
+        publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
     </Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
