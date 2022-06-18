2e28dcde00effc8581b85ae33f6127127e1486d8
rabeeh <rabeeh003@gmail.com>
2022-06-18 12:33:47 +0530

    background mobail items and nav items is ready

--- src/components/screens/welcome/Home/Mobile.js
+++ src/components/screens/welcome/Home/Mobile.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import {  Link } from "react-router-dom";
-import Logo from "./mobile.jpg";
+// import Logo from "./mobile.jpg";
 import './Home.css'
 
 function Mobile() {
   return (
+    <div className = "mobile-container">
+      <h1 className="title">Knolsort</h1>
     <div className="left">
-        <h1 className="title">Knolsort</h1>
-        <img className="mobile" src={Logo} />
-        <Link to="/" className="btn0">Home</Link>
-        <Link to="About" className="btn1">About</Link>
-        <Link to="Contact" className="btn2">Contact</Link>
+        {/* <img className="mobile" src={Logo} alt="mobail-pic"/> */}
+        <div className='mob-links'>
+          <Link to="/" className="btn0">Home</Link>
+          <Link to="About" className="btn1">About</Link>
+          <Link to="Contact" className="btn2">Contact</Link>
+        </div>
         <Link to="What" className="ques">What is Knolsort?</Link>
         <Link  to="Benfit" className="ques1">What is Benfit of this?</Link>
         <Link to="Use" className="ques2">How to Use</Link>
+        <Link to={"/signin"} className="btn-login">Login</Link>
+        <Link to={"/home"} className="btn-signin">Sign In</Link>
       </div>
+      </div>
+
+
   )
 }
 
