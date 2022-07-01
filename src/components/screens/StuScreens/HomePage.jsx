50d5d7d8550c1cdfb6a1c602cd47e19b35969fc4
rabeeh <rabeeh003@gmail.com>
2022-07-01 10:57:10 +0530

    side bar sett

--- src/components/screens/StuScreens/HomePage.jsx
+++ src/components/screens/StuScreens/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../includes/navbar/Navbar'
+import Sidebar from '../../includes/navbar/Sidebar'
 import Home from '../../pages/Students/Home/Home'
 
 function HomePage() {
   return (
     <>
         <Navbar/>
+        <Sidebar/>
         <Home/>
     </>
   )
