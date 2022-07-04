5a66b95a0b15d4c8fcbc2c5a8bc8dfd76e0c6988
Adhil <amazementtech521@gamil.com>
2022-07-04 18:57:17 +0530

    community btn

--- src/components/screens/StuScreens/CoursePage.jsx
+++ src/components/screens/StuScreens/CoursePage.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import Navbar from '../../includes/navbar/Navbar'
 import Sidebar from '../../includes/navbar/Sidebar'
+import Card from '../../includes/coscard/Card'
+import './CourcePage.css'
+import { Link } from 'react-router-dom'
+
 
 function CoursePage() {
   return (
-    <div>
+    <Fragment>
         <Navbar/>
         <Sidebar/>
-        <CoursePageBanner></CoursePageBanner>
-        <CourseName>Cource Promo</CourseName>
-        <CourseDistributer>@distruputer name</CourseDistributer>
-    </div>
+        <CourseName className='promo'>Cource Name</CourseName>
+        <CourseDistributer className='name'>@distruputer name</CourseDistributer>
+        <Link to={"/community"}>
+          <button className='right'>Join Communty</button>
+          </Link>
+          
+         
+     </Fragment>
   )
 }
 
@@ -23,9 +31,12 @@ const CoursePageBanner = styled.div`
 `
 const CourseName = styled.h1`
   padding-left: 10px;
+  
 `
 const CourseDistributer = styled.p`
   padding-left: 20px;
+  
 `
+
 
 export default CoursePage
