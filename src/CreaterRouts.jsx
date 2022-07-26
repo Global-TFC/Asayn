e4f97bf677fa12b6c4ff28feb64f0c19784fabac
rabeeh003 <rabimon003@gmail.com>
2022-07-26 21:31:02 +0530

    creater side and nav  bar and accadamy home page set

--- src/CreaterRouts.jsx
+++ src/CreaterRouts.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {  Routes, Route } from "react-router-dom";
+import CrNavbar from './components/includes/navbar/CrNavbar';
+import CrSidebar from './components/includes/navbar/CrSidebar';
 
 //accadamy page
 import Accadamypage from './components/pages/Accadamy/Accadamypage';
@@ -7,6 +9,8 @@ import Accadamypage from './components/pages/Accadamy/Accadamypage';
 function CRF() {
   return (
     <>
+        <CrNavbar/>
+        <CrSidebar/>
         <Routes>
           <Route index element={<Accadamypage/>} />
         </Routes>
---
+++ src/components/includes/navbar/CrSidebar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./Navbar.css"
+import "../../../theme/GlobalStyles"
+import { Link } from 'react-router-dom';
+
+function CrSidebar() {
+    return (
+        <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+                <li className="nav__item">
+                    <Link to={""}  className="nav__link active-link">
+                        <i className='bx bxs-home nav__icon'></i><br/>
+                        <span className="nav__name">Home</span>
+                    </Link>
+                </li>
+
+                <li class="nav__item">
+                    <Link to={"/mycourse"} className="nav__link">
+                        <i className='bx bx-chalkboard nav__icon'></i><br/>
+                        <span className="nav__name">Class</span>
+                    </Link>
+                </li>
+
+                <li className="nav__item">
+                    <Link to={"/chat"} className="nav__link">
+                        <i className='bx bx-chat nav__icon'></i><br/>
+                        <span className="nav__name">Chat</span>
+                    </Link>
+                </li>
+
+                <li className="nav__item">
+                    <Link to={"/aboutme"} className="nav__link">
+                        <i className='bx bxs-user nav__icon'></i><br/>
+                        <span className="nav__name">About</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default CrSidebar;
